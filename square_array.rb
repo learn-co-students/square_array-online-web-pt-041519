fcf349c008a0ac0f4a80b0a569a669213f1ea7cf
zhenggao cai <caizhengao1992@gmail.com>
2019-04-17 06:19:46 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
   # your code here
+  
+  new_array=[]
+  array.each do |number|
+
 end
+    n=0
+    for n in (0..array.size-1) do 
+      new_array[n]=array[n]*array[n]
+      n=+ 1
+    end
+    return new_array
+
+ end
